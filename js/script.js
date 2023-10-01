5d76b1d771821ebbd25bb6df1c0c75783c5740bb
Ezequiel L. Auge <113632140+Branca666@users.noreply.github.com>
2023-10-01 15:26:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,29 +5,39 @@ const respuestaDiv = document.getElementById('respuesta');
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-const nombre = document.getElementById('nombre').value;
-const apellido = document.getElementById('apellido').value;
-const fecha = document.getElementById('fecha').value;
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const fecha = document.getElementById('fecha').value;
 
-fetch('https://jsonplaceholder.typicode.com/users',{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    // Función para convertir todas las claves a minúsculas
+    function keysToLowerCase(obj) {
+        const result = {};
+        for (const key in obj) {
+            if (Object.hasOwnProperty.call(obj, key)) {
+                result[key.toLowerCase()] = obj[key];
+            }
+        }
+        return result;
+    }
 
-    body: JSON.stringify({
+    const dataToSend = keysToLowerCase({
         Nombre: nombre,
         Apellido: apellido,
         Fecha: fecha,
-    }),
-})
+    });
 
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Respuesta del servidor:', data);
-        respuestaDiv.innerHTML = `<p>Respuesta del servidor:</p><pre>${JSON.stringify(data, null, 2)}</pre>`;
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
     })
-    .catch((error) => console.error('Error', error));
-
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Respuesta del servidor:', data);
+            respuestaDiv.innerHTML = `<p>Respuesta del servidor:</p><pre>${JSON.stringify(data, null, 2)}</pre>`;
+        })
+        .catch((error) => console.error('Error', error));
 });
 });
