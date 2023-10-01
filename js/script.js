06e12856b89a55438e414402c237a5af7dccaa68
Ezequiel L. Auge <113632140+Branca666@users.noreply.github.com>
2023-10-01 15:45:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('registro-form');
-    const respuestaDiv = document.getElementById('respuesta');
+    const form = document.getElementById('formulario'); // Cambia 'registro-form' a 'formulario'
+    const respuestaDiv = document.getElementById('resultado'); // Cambia 'respuesta' a 'resultado'
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
 
         const nombre = document.getElementById('nombre').value;
         const apellido = document.getElementById('apellido').value;
-        const fecha = document.getElementById('fecha').value;
+        const fecha = document.getElementById('fnac').value; // Cambia 'fecha' a 'fnac'
+
+        const formData = {
+            nombre: nombre,
+            apellido: apellido,
+            fechaNacimiento: fecha, // Cambia 'fecha' a 'fechaNacimiento'
+        };
 
         fetch('https://jsonplaceholder.typicode.com/users', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                nombre: nombre,
-                apellido: apellido,
-                fecha: fecha,
-            }),
+            body: JSON.stringify(formData),
         })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Respuesta del servidor:', data);
-                respuestaDiv.innerHTML = `<p>Respuesta del servidor:</p><pre>${JSON.stringify(data, null, 2)}</pre>`;
-            })
-            .catch((error) => console.error('Error', error));
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            respuestaDiv.innerHTML = `Se registraron los datos en la API 
+            Nombre: ${nombre} <br>
+            Apellido: ${apellido} <br>
+            Fecha de nacimiento: ${fecha}<br>`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
 });
